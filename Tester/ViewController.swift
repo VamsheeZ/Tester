4a43c3673e72246c904123f60e8236117ad9fbe1
Vellore Yamini <yamini.vellore@indoztechsol.com>
2020-04-29 10:44:50 +0530

    Initial Commit

---
+++ Tester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tester
+//
+//  Created by Apple on 29/04/20.
+//  Copyright © 2020 CCTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
